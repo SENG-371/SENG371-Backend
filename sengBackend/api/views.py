29cd4fb4622d8cd749399881bb4fdc9c6d32767b
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2023-02-20 16:05:41 -0800

    FE working

--- sengBackend/api/views.py
+++ sengBackend/api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def Index(request):
+    return HttpResponse("works")
