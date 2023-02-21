3b523b3526d03a109d0efad49f5d718b399791b4
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2023-02-21 14:03:04 -0800

    Added record_details requests

--- sengBackend/api/views.py
+++ sengBackend/api/views.py
@@ -25,23 +25,23 @@ def record_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_RESQUEST)
 
 
+@api_view(["GET", "PUT", "DELETE"])
 def record_details(request, pk):
     try:
         record = Record.objects.get(pk=pk)
 
     except Record.DoesNotExist:
-        return HTTPResponse(status=404)
+        return HTTPResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = RecordSerializer(record)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = RecordSerializer(record, data=data)
+        serializer = RecordSerializer(record, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
         record.delete()
-        return HTTPResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
