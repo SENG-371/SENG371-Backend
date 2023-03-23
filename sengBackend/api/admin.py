abc92364a0690a58fc3f129f3eddafd0a3367a86
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2023-03-22 19:35:04 -0700

    Created a custom user

--- sengBackend/api/admin.py
+++ sengBackend/api/admin.py
@@ -8,3 +8,63 @@ from .models import Record
 class RecordModel(admin.ModelAdmin):
     list_filter = "illness", "description"
     list_display = "illness", "description"
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Record, UserRecord
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "age",
+        "birthday",
+        "riskLevel",
+    )
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            ("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "age",
+                    "birthday",
+                    "riskLevel",
+                )
+            },
+        ),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+        "records",
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+
+
+admin.site.register(User, CustomUserAdmin)
+
+
+admin.site.register(Record)
+
+admin.site.register(UserRecord)
