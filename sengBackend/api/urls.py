cfce72a5a5a3d34dfe45066804271bda746b2e56
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2023-04-02 20:57:24 -0700

    Added practitioner url

--- sengBackend/api/urls.py
+++ sengBackend/api/urls.py
@@ -14,15 +14,15 @@ from .views import (
 
 urlpatterns = [
     path("users/", PatientList.as_view()),
-    path("practitioners/", PractitionerListView.as_view(), name="practitioner-list"),
     path("users/<int:pk>/", PatientDetail.as_view()),
     path("users/<int:pk>/records/", PatientRecordsView.as_view()),
     path("records/", RecordList.as_view()),
     path("records/<int:pk>/", RecordDetail.as_view()),
     path("users/register/", PatientRegistrationView.as_view()),
-    path("practitioners/register/", PractitionerRegistrationView.as_view()),
     path("records/<int:pk>/update/", RecordUpdateView.as_view()),
     path(
         "users/delete/<str:username>/", PatientDeleteView.as_view(), name="user-delete"
     ),
+    path("practitioners/", PractitionerListView.as_view(), name="practitioner-list"),
+    path("practitioners/register/", PractitionerRegistrationView.as_view()),
 ]
